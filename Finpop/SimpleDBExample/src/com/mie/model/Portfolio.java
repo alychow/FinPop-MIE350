96a1ca68964cef89a3b489f45a9c66f797a2bd5c
Alison Chow <alisonemily.chow@mail.utoronto.ca>
2016-12-06 15:04:46 -0500

    Commenting done

--- Finpop/SimpleDBExample/src/com/mie/model/Portfolio.java
+++ Finpop/SimpleDBExample/src/com/mie/model/Portfolio.java
@@ -1,13 +1,19 @@
 package com.mie.model;
 
+		/* ---------------------------------PORTFOLIO-----------------------------------*/
+	
+	/* This class stores Portfolio information of the username, company name, ticker and number of shares to be 
+	displayed onto each user's portfolio. */
+
 public class Portfolio {
 
-	//Every attribute gets an element
+	// Every attribute gets an element
 	private String username;
 	private String compName;
 	private String ticker;
 	private int numShares;
 	
+	// Initializes the Portfolio class 
 	public Portfolio(){
 		this.username = "";
 		this.compName = "";
@@ -15,6 +21,7 @@ public class Portfolio {
 		this.numShares = -1;
 	}
 	
+	// Sets portfolio class with input variables required for the portfolio to be displayed on FinPOP
 	public Portfolio(String username, String compName, String ticker, int numShares){
 		this.setCompName(compName);
 		this.setNumShares(numShares);
@@ -22,34 +29,42 @@ public class Portfolio {
 		this.setUsername(username);
 	}
 	
+	// Returns username corresponding to Portfolio
 	public String getUsername(){
 		return this.username;
 	}
 	
+	// Sets username corresponding to Portfolio 
 	public void setUsername(String s){
 		this.username = s;
 	}
 	
+	// Returns name of company invested by user
 	public String getCompName(){
 		return this.compName;
 	}
 	
+	// Sets name of company invested by user
 	public void setCompName(String s){
 		this.compName = s;
 	}
 	
+	// Returns ticker of company invested by user
 	public String getTicker(){
 		return this.ticker;
 	}
 	
+	// Sets ticker of company invested by user
 	public void setTicker(String s){
 		this.ticker = s;
 	}
 	
+	// Returns number of shares the user has invested in the company 
 	public int getNumShares(){
 		return this.numShares;
 	}
 	
+	// Sets number of shares the user has invested in the company to update from portfolio page
 	public void setNumShares(int n){
 		this.numShares = n;
 	}
