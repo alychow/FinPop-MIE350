42a7af4d241cc3ce967d480561e9bdf985e2e1e8
Alison Chow <alisonemily.chow@mail.utoronto.ca>
2016-12-06 15:41:17 -0500

    Commenting done

--- Finpop/SimpleDBExample/src/com/mie/controller/PortfolioController.java
+++ Finpop/SimpleDBExample/src/com/mie/controller/PortfolioController.java
@@ -25,46 +25,43 @@ public class PortfolioController extends HttpServlet {
 		super();
 		dao = new Query();
 	}
-	//doGet has methods for addding,updating, and deleting portfolio
+	// doGet has methods for addding,updating, and deleting portfolio
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String forward = "";
 		String action = request.getParameter("action");
 		System.out.println(action);
-		if(action.equalsIgnoreCase("listPortfolio")){
-		//if action parameter is listPortfolio forward user to /listPortfolio.jsp
+		if (action.equalsIgnoreCase("listPortfolio")) {
+		// if action parameter is listPortfolio forward user to /listPortfolio.jsp
 				String userId = (request.getParameter("userId"));
 				forward = LIST_PORTFOLIO;
 				request.setAttribute("portfolio", dao.selectUserPortfolio(userId));
 			
-		}else if(action.equalsIgnoreCase("deleteCompany")){
-		//else if action parameter is deleteCompany delete the tuple that corresponds to compName and userID from the portfolio table
-			//refresh the /listPortfolio.jsp page	
+		} else if(action.equalsIgnoreCase("deleteCompany")){
+		// else if action parameter is deleteCompany delete the tuple that corresponds to compName and userID from the portfolio table
+			// refresh the /listPortfolio.jsp page	
 				String userId = request.getParameter("userId");
 				String compName = request.getParameter("compName");
 				dao.deleteCompFromPortfolio(compName,userId);
 				forward = LIST_PORTFOLIO;
 				request.setAttribute("portfolio", dao.selectUserPortfolio(userId));
 		
-		}else if(action.equalsIgnoreCase("updateCompany")){
-		//else if action is equal to updateCompany update the tuple's number of shares corresponding to compName and userid
-			//refresh the /listPortfolio.jsp page
+		} else if(action.equalsIgnoreCase("updateCompany")){
+		// else if action is equal to updateCompany update the tuple's number of shares corresponding to compName and userid
+			// refresh the /listPortfolio.jsp page
 				String userId = request.getParameter("userid");
 				String compName = request.getParameter("compName");
 				String value = request.getParameter("number");
-				//convert the value string to an Integer
-					//JS is used to check if the value is an Integer
+				// convert the value string to an Integer
+					// JS is used to check if the value is an Integer
 				dao.updateCompFromPortfolio(compName,Integer.parseInt(value),userId);
 				forward = LIST_PORTFOLIO;
-				request.setAttribute("portfolio", dao.selectUserPortfolio(userId));
-				
+				request.setAttribute("portfolio", dao.selectUserPortfolio(userId));	
 		}
-		
-
 		RequestDispatcher view = request.getRequestDispatcher(forward);
 		view.forward(request, response);
 	}
 
-	//doPost inserts a company and the number of shares into the portfolio table with the userid
+	// doPost inserts a company and the number of shares into the portfolio table with the userID
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 	
@@ -77,13 +74,11 @@ public class PortfolioController extends HttpServlet {
 			port.setCompName(i[0]);
 			port.setTicker(i[1]);
 			port.setNumShares(Integer.parseInt(i[2]));
-		if(dao.insertPortfolio(port)){
+		if (dao.insertPortfolio(port)){
 			out.printf("Company: %s | Ticker: %s | Number of Shares: %d | \n added to User: %s",port.getCompName(),port.getTicker(),port.getNumShares(),port.getUsername());
-		}else{
-			//error message if the company & number of shares cannot be added
+		} else{
+			// error message if the company & number of shares cannot be added
 			out.printf("There was an issue adding the company to the portfolio.\nPlease try again \nCompany: %s | Ticker: %s | Number of Shares: %d | \n added to User: %s",port.getCompName(),port.getTicker(),port.getNumShares(),port.getUsername());
-		}
-		
-		
+		  }
 	}
 }
